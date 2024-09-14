e40cdf5feaff458903dc98f93e00d66643e58e48
tawainfer <tawainfer@gmail.com>
2024-09-14 19:45:17 +0900

    steamの文字化けを改善

--- modules/steam.nix
+++ modules/steam.nix
@@ -1,35 +1,16 @@
-_: {
+{pkgs, ...}: {
   programs.steam = {
     enable = true;
     remotePlay.openFirewall = true;
     dedicatedServer.openFirewall = true;
   };
 
-  fonts = {
-    fontDir.enable = true;
-    fonts = with pkgs; [
-      migu
-    ];
-
-    fontconfig = {
-      localConf = ''
-        <?xml version="1.0"?>
-        <!DOCTYPE fontconfig SYSTEM "urn:fontconfig:fonts.dtd">
-        <fontconfig>
-          <description>Change default fonts for Steam client</description>
-          <match>
-            <test name="prgname">
-              <string>steamwebhelper</string>
-            </test>
-            <test name="family" qual="any">
-              <string>sans-serif</string>
-            </test>
-            <edit mode="prepend" name="family">
-              <string>Migu 1P</string>
-            </edit>
-          </match>
-        </fontconfig>
-      '';
+  nixpkgs.config.packageOverrides = pkgs: {
+    steam = pkgs.steam.override {
+      extraPkgs = pkgs:
+        with pkgs; [
+          ipaexfont
+        ];
     };
   };
 }
